02a16b5133478f5bf248c4eb669626cde97b4647
Juan José G.R <juanjoseg.r.r107@gmail.com>
2025-11-12 14:18:45 -0500

    Manejo de errores para evitar error 500 server error

--- resources/views/Productos/index.blade.php
+++ resources/views/Productos/index.blade.php
@@ -8,17 +8,17 @@
             <div class="card bg-base-100 w-85 shadow-xl">
 
                 <figure>
-                    <img src="https://loremflickr.com/250/250/motorcycle,{{ urlencode($producto->nombre) }}?lock={{ $producto->id }}"
-                        alt="{{ $producto->nombre }} " />
+                    <img src="https://loremflickr.com/250/250/motorcycle,{{ urlencode($producto->Nombre ?? 'Sin-nombre') }}?lock={{ $producto->id }}"
+                        alt="{{ $producto->Nombre ?? 'Producto sin nombre'}} " />
                 </figure>
                 <div class="card-body">
                     <h2 class="card-title">
-                        <div class="label-name">{{ $producto->Nombre }}</div>
+                        <div class="label-name">{{ $producto->Nombre ?? 'Producto desconocido' }}</div>
                     </h2>
                     <h2 class="card-title">
-                        <div class="badge label-price">$ {{ $producto->Precio }}</div>
+                        <div class="badge label-price">$ {{ $producto->Precio ?? 0 }}</div>
                     </h2>
-                    <p>Vendido por: {{ $producto->Proveedor }}</p>
+                    <p>Vendido por: {{ $producto->Proveedor ?? 'Desconocido'}}</p>
                     <div class="card-actions justify-end">
 
                         <a href="{{ route('productos.edit', $producto->id) }}" class="btn btn-sm btn-edit">Editar</a>
