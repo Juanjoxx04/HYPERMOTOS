8507b50353f306b07350495cdef56d1f9ac9aa0c
Juan José G.R <juanjoseg.r.r107@gmail.com>
2025-11-26 23:21:57 -0500

    Se asignan las imagenes reales y acordes a cada producto

--- resources/views/Productos/index.blade.php
+++ resources/views/Productos/index.blade.php
@@ -2,24 +2,26 @@
 @section('titulo', 'Hypermotos/productos')
 @section('contenido')
 
-    <div class="grid grid-cols-1 sm:grid-col-2 md:grid-cols-3 lg:grid-cols-4 gap-4 m-6">
+    <div class="grid grid-cols-1 sm:grid-col-2 md:grid-cols-2 lg:grid-cols-3 gap-12 m-10">
 
         @foreach ($productos as $producto)
-            <div class="card bg-base-100 w-85 shadow-xl">
+            <div class="card bg-white w-85 shadow-sm">
 
                 <figure>
-                    <img src="https://loremflickr.com/250/250/motorcycle,{{ urlencode($producto->Nombre ?? 'Sin-nombre') }}?lock={{ $producto->id }}"
-                        alt="{{ $producto->Nombre ?? 'Producto sin nombre'}} " />
+                    <img src="{{ $producto->imagen_url }}"
+                        alt="{{ $producto->Nombre ?? 'Producto sin nombre'}}"/>
                 </figure>
                 <div class="card-body">
                     <h2 class="card-title">
                         <div class="label-name">{{ $producto->Nombre ?? 'Producto desconocido' }}</div>
                     </h2>
-                    <h2 class="card-title">
-                        <div class="badge label-price">$ {{ $producto->Precio ?? 0 }}</div>
+                    <p class="text-black ">Vendido por: {{ $producto->Proveedor ?? 'Desconocido'}}</p>
+
+                    <h2 class="card-actions justify-end">
+                        <div class="label-price">$ {{ number_format($producto->Precio, 0, ',','.') }}</div>
                     </h2>
-                    <p>Vendido por: {{ $producto->Proveedor ?? 'Desconocido'}}</p>
-                    <div class="card-actions justify-end">
+                    
+                    <div class="card-actions justify-end mt-8">
 
                         <a href="{{ route('productos.edit', $producto->id) }}" class="btn btn-sm btn-edit">Editar</a>
 
